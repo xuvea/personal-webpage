d48c7947a3eaf542baddaf7d5dc0e82d5963184d
xuvea <90873349+xuvea@users.noreply.github.com>
2022-06-06 02:37:59 -0700

    Added Resume

---
+++ webpage.js
@@ -0,0 +1,23 @@
+
+const sections = document.querySelectorAll('section');
+
+const navi = document.querySelectorAll('a.menu-item');
+// Add an event listener listening for scroll
+window.addEventListener('scroll', () => {
+  let current = '';
+
+  sections.forEach( section => {
+    const sectionTop = section.offsetTop;
+    if(scrollY >= (sectionTop)){
+      current = section.getAttribute('id');
+    }
+  })
+  
+  navi.forEach( a => {
+    a.classList.remove('highlight');
+    if(a.classList.contains(current)){
+      a.classList.add('highlight')
+    }
+  })
+})
+
